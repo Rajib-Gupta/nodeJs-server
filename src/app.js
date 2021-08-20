e8f83cb31e2ddeea54048e225d22ec16be918327
Rajib Gupta <84308590+Rajib-Gupta@users.noreply.github.com>
2021-08-20 21:22:49 +0530

    edit connection.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = require("./routes/api");
 const cors = require("cors");
 const morgan = require("morgan");
-require("./database/connection")
 
 const app = express();
 require("./database/connection");
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql2");
+const appSettings=require('../config/appSetting')
 const { promisify } = require("util");
 
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "employee",
-  port: 3307,
+  host: appSettings.Mysql.HOST,
+  user: appSettings.Mysql.USER,
+  database: appSettings.Mysql.DB,
+  port: appSettings.Mysql.PORT,
 });
 const query = promisify(connection.query).bind(connection);
 

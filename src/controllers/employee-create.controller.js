a2698324ee0879c85e2aebd0cb5132bf7dda5af9
Rajib Gupta <84308590+Rajib-Gupta@users.noreply.github.com>
2021-08-21 17:54:01 +0530

    add employee sup_id changes

--- src/controllers/employee-create.controller.js
+++ src/controllers/employee-create.controller.js
@@ -3,6 +3,7 @@ const { query } = require("../database/connection");
 
 const employeeCreate = async (req, res) => {
   var { f_name, l_name, email, phone, role, password, sup_id } = req.body;
+  sup_id= Number(sup_id)===0?0:sup_id;
   try {
     const sql = `insert into employee_master(f_name,l_name,phone,email,role,password,sup_id) values ("${f_name}","${l_name}","${email}","${phone}","${role}","${password}","${sup_id}")`;
     const insertedData = await query(sql);

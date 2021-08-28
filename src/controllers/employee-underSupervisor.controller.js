ccecd4a0e6c9e402fcf47c4f7ab1a61384c8217a
Rajib Gupta <84308590+Rajib-Gupta@users.noreply.github.com>
2021-08-28 18:16:36 +0530

    employee under api changes

--- src/controllers/employee-underSupervisor.controller.js
+++ src/controllers/employee-underSupervisor.controller.js
@@ -5,7 +5,7 @@ const employeeUnderSuper= async (req, res) => {
     var email = req.body.email;
     try {
       var sql = `
-      SELECT kp.sup_id,kp.feedback_emp_id, em.* FROM employee_master as e LEFT JOIN employee_master as em on em.sup_id = e.emp_id and e.emp_id!=em.emp_id LEFT JOIN employee_kpi as kp on kp.emp_id = em.emp_id and kp.emp_id != kp.feedback_emp_id where e.email="${email}"`;
+      SELECT kp.sup_id,kp.feedback_emp_id, em.* FROM employee_master as e LEFT JOIN employee_master as em on em.sup_id = e.emp_id and e.emp_id!=em.emp_id LEFT JOIN employee_kpi as kp on kp.emp_id = em.emp_id and kp.emp_id != kp.feedback_emp_id where e.email="${email}" and em.emp_id is not null`;
       var rows =  await query(sql);
       var jsonstr = JSON.stringify(rows);
       res.end(jsonstr);
